27225d7ce1d219697ef7dc133fd9f0422a4abd16
Chart.js <chartjs-ci@users.noreply.github.com>
2019-03-04 08:17:08 +0000

    Deploy master from chartjs/Chart.js
    Commit: 344628ba9c5fc95a1edbc9cf94488461d24a4b32

--- dist/master/Chart.bundle.js
+++ dist/master/Chart.bundle.js
@@ -2917,20 +2917,24 @@ var core_animations = {
 	 */
 	advance: function() {
 		var animations = this.animations;
-		var animation, chart;
+		var animation, chart, numSteps, nextStep;
 		var i = 0;
 
+		// 1 animation per chart, so we are looping charts here
 		while (i < animations.length) {
 			animation = animations[i];
 			chart = animation.chart;
+			numSteps = animation.numSteps;
 
-			animation.currentStep = Math.floor((Date.now() - animation.startTime) / animation.duration * animation.numSteps);
-			animation.currentStep = Math.min(animation.currentStep, animation.numSteps);
+			// Make sure that currentStep starts at 1
+			// https://github.com/chartjs/Chart.js/issues/6104
+			nextStep = Math.floor((Date.now() - animation.startTime) / animation.duration * numSteps) + 1;
+			animation.currentStep = Math.min(nextStep, numSteps);
 
 			helpers$1.callback(animation.render, [chart, animation], chart);
 			helpers$1.callback(animation.onAnimationProgress, [animation], chart);
 
-			if (animation.currentStep >= animation.numSteps) {
+			if (animation.currentStep >= numSteps) {
 				helpers$1.callback(animation.onAnimationComplete, [animation], chart);
 				chart.animating = false;
 				animations.splice(i, 1);
