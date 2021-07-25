3b9364b1be627891f098f71b569132958905e50f
Chart.js <chartjs-ci@users.noreply.github.com>
2021-07-25 12:00:12 +0000

    Deploy master from chartjs/Chart.js
    Commit: a8337a32c976393e4648de633dcc49cdec2760b3

--- dist/master/chart.js
+++ dist/master/chart.js
@@ -1,5 +1,5 @@
 /*!
- * Chart.js v3.4.1
+ * Chart.js v3.5.0
  * https://www.chartjs.org
  * (c) 2021 Chart.js Contributors
  * Released under the MIT License
@@ -6509,7 +6509,7 @@ function needContext(proxy, names) {
   return false;
 }
 
-var version = "3.4.1";
+var version = "3.5.0";
 
 const KNOWN_POSITIONS = ['top', 'bottom', 'left', 'right', 'chartArea'];
 function positionIsHorizontal(position, axis) {
