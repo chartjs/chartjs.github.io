631ebc36241910f9e4038239941dfa2e6610e6d0
Chart.js <chartjs-ci@users.noreply.github.com>
2021-12-05 13:59:56 +0000

    Deploy master from chartjs/Chart.js
    Commit: cd9d5289a49a9252a230b749acf34823382d99a4

--- dist/master/chart.esm.js
+++ dist/master/chart.esm.js
@@ -6236,16 +6236,17 @@ function drawFullCircleBorders(ctx, element, inner) {
 }
 function drawBorder(ctx, element, offset, spacing, endAngle) {
   const {options} = element;
+  const {borderWidth, borderJoinStyle} = options;
   const inner = options.borderAlign === 'inner';
-  if (!options.borderWidth) {
+  if (!borderWidth) {
     return;
   }
   if (inner) {
-    ctx.lineWidth = options.borderWidth * 2;
-    ctx.lineJoin = 'round';
+    ctx.lineWidth = borderWidth * 2;
+    ctx.lineJoin = borderJoinStyle || 'round';
   } else {
-    ctx.lineWidth = options.borderWidth;
-    ctx.lineJoin = 'bevel';
+    ctx.lineWidth = borderWidth;
+    ctx.lineJoin = borderJoinStyle || 'bevel';
   }
   if (element.fullCircles) {
     drawFullCircleBorders(ctx, element, inner);
@@ -6338,6 +6339,7 @@ ArcElement.id = 'arc';
 ArcElement.defaults = {
   borderAlign: 'center',
   borderColor: '#fff',
+  borderJoinStyle: undefined,
   borderRadius: 0,
   borderWidth: 2,
   offset: 0,
