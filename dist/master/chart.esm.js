83d3a11d93faec27fa02defa23b2e53da264bd7e
Chart.js <chartjs-ci@users.noreply.github.com>
2022-05-22 13:53:43 +0000

    Deploy master from chartjs/Chart.js
    Commit: 1422c93dc0c9471abc54f4c9ed4d9826239a05af

--- dist/master/chart.esm.js
+++ dist/master/chart.esm.js
@@ -2204,6 +2204,22 @@ class PolarAreaController extends DatasetController {
     this._updateRadius();
     this.updateElements(arcs, 0, arcs.length, mode);
   }
+  getMinMax() {
+    const meta = this._cachedMeta;
+    const range = {min: Number.POSITIVE_INFINITY, max: Number.NEGATIVE_INFINITY};
+    meta.data.forEach((element, index) => {
+      const parsed = this.getParsed(index).r;
+      if (!isNaN(parsed) && this.chart.getDataVisibility(index)) {
+        if (parsed < range.min) {
+          range.min = parsed;
+        }
+        if (parsed > range.max) {
+          range.max = parsed;
+        }
+      }
+    });
+    return range;
+  }
   _updateRadius() {
     const chart = this.chart;
     const chartArea = chart.chartArea;
