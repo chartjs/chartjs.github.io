76287cd41ef2d90d86e8b34d081ae273a9627a23
Chart.js <chartjs-ci@users.noreply.github.com>
2023-02-10 06:37:26 +0000

    Deploy master from chartjs/Chart.js
    Commit: cfefb3d47fe9e99a591d02c359f8492fc9bacf17

--- dist/master/chart.js
+++ dist/master/chart.js
@@ -1,5 +1,5 @@
 /*!
- * Chart.js v4.2.0
+ * Chart.js v4.2.1
  * https://www.chartjs.org
  * (c) 2023 Chart.js Contributors
  * Released under the MIT License
@@ -5466,7 +5466,7 @@ function needContext(proxy, names) {
     return false;
 }
 
-var version = "4.2.0";
+var version = "4.2.1";
 
 const KNOWN_POSITIONS = [
     'top',
