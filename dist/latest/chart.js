9f1fd7834ce5b73f65ff8c15e7933e92982bc83b
Chart.js <chartjs-ci@users.noreply.github.com>
2023-07-26 15:50:23 +0000

    Deploy 4.3.2 from chartjs/Chart.js
    Commit: c2feec5c31f75d0341d9eef924c1cc78af85c0c1

--- dist/latest/chart.js
+++ dist/latest/chart.js
@@ -1,5 +1,5 @@
 /*!
- * Chart.js v4.3.1
+ * Chart.js v4.3.2
  * https://www.chartjs.org
  * (c) 2023 Chart.js Contributors
  * Released under the MIT License
@@ -5499,7 +5499,7 @@ function needContext(proxy, names) {
     return false;
 }
 
-var version = "4.3.1";
+var version = "4.3.2";
 
 const KNOWN_POSITIONS = [
     'top',
